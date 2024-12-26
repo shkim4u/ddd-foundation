5e6eb2a2f8f69df0260d041a5f9ba0d44e8818e7
Sanghyoun Kim <sanghyou@amazon.com>
2024-12-27 07:43:28 +0900

    Inventory sample data

--- src/main/resources/db/migration/sample/R__Add_sample_inventory_data.sql
+++ src/main/resources/db/migration/sample/R__Add_sample_inventory_data.sql
@@ -1,5 +1,5 @@
 use shop;
 
-insert into inventory (id, product_id, price, quantity) values ('inv-001', 'prod-001', 50000, 3);
-insert into inventory (id, product_id, price, quantity) values ('inv-002', 'prod-002', 10000, 2);
-insert into inventory (id, product_id, price, quantity) values ('inv-003', 'prod-003', 5000, 2);
+insert into inventory (id, product_id, price, quantity) values ('inv-001', 'prod-001', 50000, 2);
+insert into inventory (id, product_id, price, quantity) values ('inv-002', 'prod-002', 10000, 1);
+insert into inventory (id, product_id, price, quantity) values ('inv-003', 'prod-003', 5000, 1);
